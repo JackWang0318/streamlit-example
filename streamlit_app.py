f07c49709643c82e05fac76299066e5a4c93cd90
JackWang0318 <whj_20170318@163.com>
2023-04-13 13:44:09 +0800

    change1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,23 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+import numpy as np
+import pandas as pd
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+st.markdown("""# This is a header
+## This is a sub header
+This is text""")
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+df = pd.DataFrame({
+          'first column': list(range(1, 11)),
+          'second column': np.arange(10, 101, 10)
+        })
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+# this slider allows the user to select a number of lines
+# to display in the dataframe
+# the selected value is returned by st.slider
+line_count = st.slider('Select a line count', 1, 10, 3)
 
+# and used in order to select the displayed lines
+head_df = df.head(line_count)
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+head_df
